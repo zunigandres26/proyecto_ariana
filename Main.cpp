3129f89f9006b8de9e9566006677215b87ef9b33
Andres Zuniga <53869945+zunigandres26@users.noreply.github.com>
2021-11-26 22:58:01 -0600

    ipad to laptop

--- Main.cpp
+++ Main.cpp
@@ -3,23 +3,57 @@
 using namespace std;
 
 void readMatrix(double [],int &, int &,char []);
+void WriteOnFile(char []);
+void WriteArray(double [], int , int );
+void multiplicacionMatrices(int );
+
 
 int main(){
-	int nrows, ncols;
-	double A[100*100];
-	readMatrix(A,nrows,ncols,"matriz.txt");
+	int nrows = 4, ncols = 4;
+	double A[4*4];
+	double B[4*4];
+	
+	readMatrix(A,nrows,ncols,"matrizA.txt");
+	readMatrix(B,nrows,ncols,"matrizB.txt");
+
 	cout<<"Numero de filas: "<<nrows<<endl;
 	cout<<"Numero de columnas: "<<ncols<<endl;
 	
 	for(int i=0;i<nrows*ncols;i++){
 		cout<<A[i]<<"\t";
 	}
-	
-	
 	return 0;
 }
 
-void readMatrix(double M[],int &nrows, int &ncols,char filename[]){
+void multiplicacionMatrices(int n){
+	if (n == 1){
+
+	}
+	else{
+		WriteOnFile("las dimensiones de las matrices no permiten efectuar la multiplicacion");
+	}
+}
+
+void WriteArray(double C[], int nrows, int ncols){
+	ofstream archivo;
+	archivo.open("matrizAB.txt");
+	archivo<<nrows<<endl;
+	archivo<<ncols<<endl;
+	for(int i=0; i<nrows; i++){
+		for(int j=0; j<ncols; j++){
+			archivo<<C[i*ncols+j]<<" ";
+		}
+		archivo<<endl;
+	}
+}
+
+void WriteOnFile(char msj[]){
+	ofstream archivo;
+	archivo.open("matrizAB.txt");
+	archivo<<msj<<".";
+}
+
+void readMatrix(double M[],int &nrows, int &ncols, char filename[]){
 	ifstream file (filename);
 	file >> nrows;
 	file >> ncols; 
